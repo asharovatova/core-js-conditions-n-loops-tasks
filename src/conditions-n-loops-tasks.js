50c80bd89a42b9dbe01f0481fa51d5018628687c
asharovatova <co.rin@mail.ru>
2024-12-10 02:07:42 +0300

    feat: implement convertToRomanNumerals

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -121,8 +121,31 @@ function isIsoscelesTriangle(a, b, c) {
  *  10  => X
  *  26  => XXVI
  */
-function convertToRomanNumerals(/* num */) {
-  throw new Error('Not implemented');
+function convertToRomanNumerals(num) {
+  const map = {
+    1: 'I',
+    2: 'II',
+    3: 'III',
+    4: 'IV',
+    5: 'V',
+    6: 'VI',
+    7: 'VII',
+    8: 'VIII',
+    9: 'IX',
+    10: 'X',
+  };
+
+  let roman = '';
+
+  if (num >= 10) {
+    for (let i = 1; i <= num / 10; i += 1) {
+      roman += 'X';
+    }
+  }
+
+  roman += map[num % 10] || '';
+
+  return roman;
 }
 
 /**
